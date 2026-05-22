bff5f06f98fa8527fdad2f0cd7538c531479a1a0
Hetu <sahu.hetram@gmail.com>
2026-05-22 10:16:42 +0530

    test case for DT,DL & DQ done

--- test_scripts/test_data_quality.py
+++ test_scripts/test_data_quality.py
@@ -22,13 +22,16 @@ logger = logging.getLogger(__name__)
 @pytest.mark.usefixtures("connect_to_mysql_database")
 class TestDataQuality:
     validation_utility = ValidationUtility()
-    data_quality_utility = DataQualityUtility
+    data_quality_utility = DataQualityUtility()
 
     def test_data_quality_duplicate_check_for_product_data_csv_file(self,connect_to_mysql_database):
         try:
             test_case_name = inspect.currentframe().f_code.co_name
+            logger.info(f"test case : {test_case_name} started ...")
             duplicate_status = self.data_quality_utility.check_duplicate_in_file("test_data/product_data_from_linux.csv","csv")
-            print(duplicate_status)
+            logger.info(f"Duplicate status is : {duplicate_status}")
             assert duplicate_status is True,"There are duplicate in the product file"
+            logger.info(f"test case : {test_case_name} completed ...")
         except Exception as e:
             logger.error(f"error while performing duplicate check {e}")
+            pytest.fail()
